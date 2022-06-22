5ddd5cf7cd040fe95a8fee2c82a1194cc46c5ff7
Sedthinee888 <Sedthinee888@LAPTOP-EJDB9TSI>
2022-06-23 00:45:10 +0700

    remove annotation

--- chapter1-spring-xml-config/src/main/java/cc/magickiat/spring/intro/config/DaosConfig.java
+++ chapter1-spring-xml-config/src/main/java/cc/magickiat/spring/intro/config/DaosConfig.java
@@ -1,12 +1,10 @@
 package cc.magickiat.spring.intro.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import cc.magickiat.spring.intro.dao.AccountDao;
 import cc.magickiat.spring.intro.dao.AccountFakeDao;
 import cc.magickiat.spring.intro.dao.AccountJdbcDao;
 
-@Configuration
 public class DaosConfig {
 
   @Bean
--- chapter1-spring-xml-config/src/main/java/cc/magickiat/spring/intro/config/ServicesConfig.java
+++ chapter1-spring-xml-config/src/main/java/cc/magickiat/spring/intro/config/ServicesConfig.java
@@ -2,11 +2,9 @@ package cc.magickiat.spring.intro.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import cc.magickiat.spring.intro.dao.AccountDao;
 import cc.magickiat.spring.intro.service.AccountService;
 
-@Configuration
 public class ServicesConfig {
   @Bean
   public AccountService accountService(@Qualifier("accountFakeDao") AccountDao accountDao) {
